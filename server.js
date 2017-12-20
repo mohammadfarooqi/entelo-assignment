14c43b7661286045740306a5dde8c44277076ed1
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-12-20 12:11:16 -0500

    /sendemail route complete... added comments

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 // Notes for improvements
 //    1) add Joi for validating inputs for routes
 //    2) split this file into multiple files (ex. routes, server, etc...)
+//    3) maybe use POST instead of get with params to send email (make call to elastic api)
+
+// Import env vars
+require('dotenv').config({ silent: true });
 
 // Imports 
 const Hapi = require('hapi');
 const Good = require('good');
+const Wreck = require('wreck');
 
 // Create instance of Server
 const server = new Hapi.Server({ debug: { request: ['error'] } });
 
 // Server connection settings
 server.connection({
-  host: 'localhost',
-  port: 3000
+  host: process.env.SERVER_HOST || '0.0.0.0',
+  port: process.env.PORT || '3000',
 });
 
 // Routes: 
@@ -27,13 +32,31 @@ server.route({
 server.route({
   method: 'POST',
   path: '/sendEmail',
-  handler: (req, res) => {
+  handler: async (req, res) => {
     // Body details for email 
     const to = req.payload.to;
     const subject = req.payload.subject;
     const body = req.payload.body;
 
-    res('to = ' + to + ' subject = ' + subject + ' body = ' + body);
+    // http get call to elastic email api
+    const { resp, payload } = await Wreck.get('http://api.elasticemail.com/v2/email/send?apikey=' + process.env.ELASTIC_EMAIL_API_KEY + '&from=mohammad.farooqi@gmail.com&subject=' + encodeURI(subject) + '&to=' + encodeURI(to) + '&bodyHtml=' + encodeURI(body));
+
+    console.log(payload.toString());
+
+    // console.log(JSON.stringify({
+    //   sent: 'to = ' + to + ' subject = ' + subject + ' body = ' + body,
+    //   resp: resp,
+    //   pay: payload.toString()
+    // }));
+
+    // converting buffer to str
+    const payloadJSON = JSON.parse(payload.toString());
+
+    if (payloadJSON && payloadJSON.success) {
+      return res(payloadJSON).code(200);
+    } else if (payloadJSON && !payloadJSON.success) {
+      return res(payloadJSON).code(400);
+    }
   }
 });
 
