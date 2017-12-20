fbfca384b97a9d0dfa5dd549c2fb20c6240607ec
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-12-19 21:20:52 -0500

    added logging and configured basic post route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
+const Good = require('good');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({ debug: { request: ['error'] } });
 
 server.connection({
   host: 'localhost',
@@ -11,14 +12,53 @@ server.route({
   method: 'GET',
   path: '/',
   handler: (req, res) => {
-    res('Hello World');
+    res('/ route working!');
   }
 });
 
-server.start(err => {
-  if (err) {
-    throw err;
+server.route({
+  method: 'POST',
+  path: '/sendEmail',
+  handler: (req, res) => {
+    res('request body ' + JSON.stringify(req.payload));
   }
+});
 
-  console.log('Server running on ' + server.info.uri);
-})
+server.register(
+  {
+    register: Good,
+    options: {
+      reporters: {
+        console: [
+          {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [
+              {
+                response: '*',
+                log: '*'
+              }
+            ]
+          },
+          {
+            module: 'good-console'
+          },
+          'stdout'
+        ]
+      }
+    }
+  },
+  err => {
+    if (err) {
+      throw err;
+    }
+
+    server.start(err => {
+      if (err) {
+        throw err;
+      }
+    
+      server.log('info', 'Server running at: ' + server.info.uri);      
+    });
+  }
+);
